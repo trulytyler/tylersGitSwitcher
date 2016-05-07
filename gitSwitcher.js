df84efad0949cadf78bc8eb227792ba1505da232
“trulytylergit config --global user.email “tylerharrisdesign@gmail.com <YOUR@EMAIL.com>
2016-05-07 00:35:47 -0700

    gitswitcher

---
+++ gitSwitcher.js
@@ -0,0 +1,31 @@
+
+
+
+//TYLERS SWITCH STMT 
+
+var daysOfTheWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+
+switch(daysOfTheWeek) {
+	case 'Monday':
+	console.log("someones got a case of the mondays");
+break;
+case 'Tuesday':
+    console.log("taco tuesday");
+    break;
+    case 'Wednesday':
+    console.log ("Ugh....fuck");
+	break;
+	case 'Thursday':
+	console.log ('yay! general assembly day!');
+	break;
+	case 'Friday':
+	console.log ('Im gonna eat dinner and sleep so hard tonight!');
+	break;
+	case 'Saturday':
+	console.log ('YYYEEEZZZZ!!!!1 ITS SATURDAY');
+	break;
+	case 'Sunday':
+	console.log ('bed bath and beyond and a glass of orange juice');
+
+}
