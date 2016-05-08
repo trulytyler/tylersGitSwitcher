d3277cef564c262ceea437fc6131cd80139687cf
“trulytylergit config --global user.email “tylerharrisdesign@gmail.com <YOUR@EMAIL.com>
2016-05-08 12:57:48 -0700

    added document write capability

--- gitSwitcher.js
+++ gitSwitcher.js
@@ -1,30 +1,41 @@
-
-
-
+<script>
 //TYLERS SWITCH STMT 
 
-var daysOfTheWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-
+var daysOfTheWeek = prompt("like omg! what day is it?");
 
 switch(daysOfTheWeek) {
 case 'Monday':
 console.log("someones got a case of the mondays");
+document.write("mondays are cool");
 break;
 case 'Tuesday':
 console.log("taco tuesday");
+document.write("tuesdays are cool");
 break;
 case 'Wednesday':
-console.log ("Ugh....fuck");
+console.log ("Ugh....");
+document.write("Wednesdays are cool");
 break;
 case 'Thursday':
 console.log ('yay! general assembly day!');
+document.write("Thursdays are cool");
 break;
 case 'Friday':
 console.log ('Im gonna eat dinner and sleep so hard tonight!');
+document.write("Fridays are cool");
 break;
 case 'Saturday':
 console.log ('YYYEEEZZZZ!!!!1 ITS SATURDAY');
+document.write("Saturdays are cool");
 break;
 case 'Sunday':
 console.log ('bed bath and beyond and a glass of orange juice');
+document.write("mondays are cool");
+break;
+default:
+document.write("What day are you trying to find? Days must have the first letter capitalized Ex: Sunday, Monday, Tuesday..etc");
 }
+</script>
+
+
+
