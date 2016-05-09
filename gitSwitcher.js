44f9513bac312b03adeb079d335009758a796cb0
“trulytylergit config --global user.email “tylerharrisdesign@gmail.com <YOUR@EMAIL.com>
2016-05-08 21:21:08 -0700

    changed docwrite to getelement

--- gitSwitcher.js
+++ gitSwitcher.js
@@ -6,36 +6,36 @@ var daysOfTheWeek = prompt("like omg! what day is it?");
 switch(daysOfTheWeek) {
 case 'Monday':
 console.log("someones got a case of the mondays");
-document.write("mondays are cool");
+document.getElementById("daytext").innerHTML = 'someones got a case of the mondays';
 break;
 case 'Tuesday':
 console.log("taco tuesday");
-document.write("tuesdays are cool");
+document.getElementById("daytext").innerHTML = 'Tuesdays are great cause tacos!';
 break;
 case 'Wednesday':
 console.log ("Ugh....");
-document.write("Wednesdays are cool");
+document.getElementById("daytext").innerHTML = 'Ugh...dont get me started';
 break;
 case 'Thursday':
 console.log ('yay! general assembly day!');
-document.write("Thursdays are cool");
+document.getElementById("daytext").innerHTML = 'yay! General assembly javascript fun time!!';
 break;
 case 'Friday':
 console.log ('Im gonna eat dinner and sleep so hard tonight!');
-document.write("Fridays are cool");
+document.getElementById("daytext").innerHTML = 'Im gonna eat dinner and sleep so hard tonight!';
 break;
 case 'Saturday':
 console.log ('YYYEEEZZZZ!!!!1 ITS SATURDAY');
-document.write("Saturdays are cool");
+document.getElementById("daytext").innerHTML = 'YYYEEEZZZZ!!!!1 ITS SATURDAY';
 break;
 case 'Sunday':
 console.log ('bed bath and beyond and a glass of orange juice');
-document.write("mondays are cool");
+document.getElementById("daytext").innerHTML = 'bed bath and beyond and a glass of orange juice';
 break;
 default:
-document.write("What day are you trying to find? Days must have the first letter capitalized Ex: Sunday, Monday, Tuesday..etc");
+document.getElementById("daytext").innerHTML = 'What day are you trying to find? Days must have the first letter capitalized Ex: Sunday, Monday, Tuesday..etc';
+
 }
 </script>
-
 
 
